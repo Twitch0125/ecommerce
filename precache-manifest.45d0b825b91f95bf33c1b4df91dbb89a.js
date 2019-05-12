986e5f49d6680ccc8286d6fe345e34c208157440
Kaleb Pop <twitcherc@gmail.com>
2019-05-11 23:50:26 -0600

    Updates

---
+++ precache-manifest.45d0b825b91f95bf33c1b4df91dbb89a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de553691399a73cb789ed61fd8a80c55",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "e37ac781b3f2b2493ee9",
+    "url": "/ecommerce/static/css/main.3dd12da4.chunk.css"
+  },
+  {
+    "revision": "ce26a04b1c0d7d77371e",
+    "url": "/ecommerce/static/js/2.df16c809.chunk.js"
+  },
+  {
+    "revision": "e37ac781b3f2b2493ee9",
+    "url": "/ecommerce/static/js/main.80d60de1.chunk.js"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  }
+]);
